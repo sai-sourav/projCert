518a72ec84ede09db1607f540c7ad8d86ccda609
PratyushaTurlapati <pratyushajuly@gmail.com>
2022-02-05 21:51:26 +0530

    script file to install docker on test server

--- website/docker-inst.sh
+++ website/docker-inst.sh
@@ -5,21 +5,10 @@ sudo apt-get install \
     curl \
     gnupg \
     lsb-release
-
-sleep 10
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-sleep 10
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sleep 30
-
 sudo apt-get update
-
-sleep 50
-
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
